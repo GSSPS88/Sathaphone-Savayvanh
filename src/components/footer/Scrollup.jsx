c8b91960a99ee648cb79bc3904439ab411419e79
GSSPS88 <sathaphonegot@gmail.com>
2024-07-22 07:18:25 +0700

    Update Scrollup.jsx
    Change some word

--- src/components/footer/Scrollup.jsx
+++ src/components/footer/Scrollup.jsx
@@ -4,7 +4,7 @@ const Scrollup = () => {
   return (
     <div className="footer__scroll">
         <a href="#home" className="footer__scroll-button button--flex">
-            <span className="footer__scroll-name">See More</span>
+            <span className="footer__scroll-name">Back to top</span>
             <i class="bi bi-caret-up-fill"></i>
         </a>
     </div>
